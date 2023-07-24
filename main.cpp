caa82be289cf520f8cc1e22809b448477dd91287
JudeaDumont <dumontjudea2@gmail.com>
2023-07-24 06:43:51 -0600

    - assert for facorypattern - note about object property based checking

--- main.cpp
+++ main.cpp
@@ -22,11 +22,14 @@ int main() {
     assert(s.getName()=="singletonChange");
     std::cout << s.getName() << std::endl;
 
-    // Factory
+    // Factory todo: integrate json object verification into test materials.
     ConcreteFactoryA cf = ConcreteFactoryA();
     std::unique_ptr<AbstractProductA> pA(cf.CreateProductA());
     std::unique_ptr<AbstractProductB> pB(cf.CreateProductB());
+    assert(pA->toString()=="{name:\"ConcreteProductA\"}");
     std::cout << pA->toString() << std::endl;
+    //todo: testing the date should just check for not null
+    //assert(pB->toString()=="{name:\"ConcreteProductB\", created:\"23-07-2023 03-17-04\"}");
     std::cout << pB->toString() << std::endl;
 
     return 0;
